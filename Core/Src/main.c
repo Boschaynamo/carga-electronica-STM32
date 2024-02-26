ccc5a4549ca1e7dbc8f07fc9adc63eeb63c932a8
Nicolas Porco <nicolasseze@gmail.com>
2024-02-26 14:14:31 -0300

    listo para presentar

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -420,7 +420,7 @@ static void MX_ADC1_Init(void)
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ENABLE;
+  hadc1.Init.ScanConvMode = DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
@@ -934,9 +934,6 @@ void StartDefaultTask(void const * argument)
 
 	/* Inicializo DAC */
 	DAC_init();
-
-	/* Inicializo ADC */
-	HAL_ADC_Start(&hadc1);
 
 	/* Inicializo Fecha en GUI */
 	//Espero 3 segundos que se inicialice GUI
